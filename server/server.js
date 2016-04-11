d20864c929b24bdd123521061cd0c431113d9969
suzannaaltman <suzannaaltman@gmail.com>
2016-04-10 20:23:26 -0500

    working so far, about to grunt

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var index = require('./routes/index');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use('/', index);
+app.use(express.static('server/public'));
+
+var server = app.listen(3000, function() {
+  var port = server.address().port;
+  console.log('Listening on port: ', port);
+});
